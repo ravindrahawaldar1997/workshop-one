5dd4c370ade2abb4519438684e0cc85be54dcb4c
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-29 20:29:38 +0530

    Added: program TicTocToe game

---
+++ TicTacToeGame.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.workshop;
+
+public class TicTacToeGame {
+	static void display() {
+	
+		char a[]=new char[10];
+		for(int i=1; i<10; i++) {
+			a[i]= ' ';		}
+	}
+
+	public static void main(String[] args) {
+		display();
+	}
+
+}
